0f72f6b3ea874cc986740e56e7b24c38d2febc30
vikasco987 <vikasvikas9095@gmail.com>
2025-11-11 14:15:23 +0530

    add

---
+++ src/app/signed_bg/page.tsx
@@ -0,0 +1,15 @@
+export default function SignedBGPage() {
+  return (
+    <div style={{ width: "100%", height: "100vh", border: "none" }}>
+      <iframe
+        src="https://zfrmz.in/7oGOIs1yHb2rUlfpiQ8K"
+        width="100%"
+        height="100%"
+        style={{
+          border: "none",
+        }}
+        title="Signed Form"
+      ></iframe>
+    </div>
+  );
+}
