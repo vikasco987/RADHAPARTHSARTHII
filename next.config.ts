9571f7ef421034f1913bc9bdbed61801cc3f0492
vikasco987 <vikasvikas9095@gmail.com>
2025-11-13 14:00:19 +0530

    ADD

--- next.config.ts
+++ next.config.ts
@@ -22,13 +22,38 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  // 👇 Add your redirect here
+  // ✅ Multiple redirects
   async redirects() {
     return [
       {
-        source: "/signed_bg",
+        source: "/sign",
+        destination: "https://zfrmz.in/UotLNR6yS254MSfFzrCb",
+        permanent: false,
+      },
+      {
+        source: "/store",
+        destination: "https://zfrmz.in/8sZaGOkzagBW8tqiF0b8",
+        permanent: false,
+      },
+      {
+        source: "/gita",
         destination: "https://zfrmz.in/7oGOIs1yHb2rUlfpiQ8K",
-        permanent: false, // use true if you want 301 permanent redirect
+        permanent: false,
+      },
+      {
+        source: "/gita_custom",
+        destination: "https://zfrmz.in/7oGOIs1yHb2rUlfpiQ8K",
+        permanent: false,
+      },
+      {
+        source: "/gitateam",
+        destination: "https://zfrmz.in/dwSUjlTEpbxoJ3M3lhDN",
+        permanent: false,
+      },
+      {
+        source: "/gita_form",
+        destination: "https://forms.gle/JhAtDeAiG9afk2mj6",
+        permanent: false,
       },
     ];
   },
